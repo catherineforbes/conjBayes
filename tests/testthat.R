1afba6b4b68801c0480f6b8271380b01f4e6ff54
Pranali Angne <pang0008@student.monash.edu>
2022-10-04 14:25:33 +1100

    testing betabernouiill and adding vignettes

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(BF1)
+library(BFone)
 
-test_check("BF1")
+test_check("BFone")
---
+++ tests/testthat/test-BetaBernoulli.R
@@ -0,0 +1,3 @@
+test_that("BetaBernoulli is wroking", {
+  expect_equal(2 * 2, 4)
+})
